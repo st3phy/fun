293b6aea83ad48900a26829e79e4bdc6f0dadfd0
Stefania Irimiea <stefania@irimiea.com>
2026-06-08 16:49:22 +0300

    [code] Arranging Coins no quadratic

--- src/85-arranging-coins.js
+++ src/85-arranging-coins.js
@@ -3,8 +3,24 @@
  *  @return {number}
  */
 const arrangeCoins = n => {
-    // The sum of the first k rows is k(k+1)/2 = n, so solve for k applying the quadratic formula
-    return Math.floor((-1 + Math.sqrt(1 + 8 * n)) / 2);
+    // The numbers that return complete rows are 0+1, 1+2, 3+3, 6+4, 10+5, 15+6 etc
+    // So we calculate the completed rows by using an i index that increment with one
+    let complete = 0;
+    let i = 1;
+
+    while (true) {
+        // If n is a complete number, return the previous index
+        if (complete === n) {
+            return i - 1;
+        }
+        // If we went over n, return the number of complete rows from the previously complete number
+        if (complete > n) {
+            return i - 2;
+        }
+
+        complete += i;
+        i++;
+    }
 };
 
 module.exports = { arrangeCoins };
